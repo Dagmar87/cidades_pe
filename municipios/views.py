43ab93420c2355cdb822d8685a9982e969e7d009
José Dagmar <dagmar.sobrinho@1telecom.com.br>
2025-10-07 10:32:57 -0300

    Coreção da url da view

--- municipios/views.py
+++ municipios/views.py
@@ -2,7 +2,7 @@ import requests
 from django.http import JsonResponse
 
 def listar_municipios(request):
-    url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/26/municipio'
+    url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/26/municipios'
     response = requests.get(url)
 
     if response.status_code == 200:
