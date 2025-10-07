d96447bf12466d32bccf1faf4dfa719db31d694e
José Dagmar <dagmar.sobrinho@1telecom.com.br>
2025-10-07 10:48:34 -0300

    Configuração dos Templates no settings.py

--- cidades_ibge/settings.py
+++ cidades_ibge/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cidades_ibge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
