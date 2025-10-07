534f17524203db22eede0e7ad7ea721dc6d5e1a7
José Dagmar <dagmar.sobrinho@1telecom.com.br>
2025-10-07 10:22:02 -0300

    Criação da View para Consumir a API

--- municipios/views.py
+++ municipios/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+def listar_municipios(request):
+    url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/26/municipio'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados = response.json()
+        cidades = [cidade['nome'] for cidade in dados]
+        return JsonResponse({'cidades': cidades})
+    else:
+        return JsonResponse({'erro': 'Não foi possível acessar a API do IBGE'}, status=500)
